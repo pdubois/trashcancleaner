686978451b9ecea42ebc9758663563450907ab2a
philippe.dubois@alfresco.com <philippe.dubois@alfresco.com>
2017-01-18 15:26:38 +0100

    Modified transaction management

--- alfrescotrashcancleaner2/src/main/java/org/alfresco/trashcan/cleaner/TrashcanCleaner.java
+++ alfrescotrashcancleaner2/src/main/java/org/alfresco/trashcan/cleaner/TrashcanCleaner.java
@@ -197,7 +197,79 @@ public class TrashcanCleaner
         }
         return false;
     }
+    int deleteRecursive(NodeRef nodeRef, int maxNumOfNodesInTransaction, MutableBoolean skip)
+    {
+        final NodeRef fNodeREf = nodeRef;
+        final MutableBoolean fSkip = skip;
+        final int fMaxNumOfNodesInTransaction = maxNumOfNodesInTransaction;
+        final RetryingTransactionCallback<Integer> recursiveWork = new RetryingTransactionCallback<Integer>()
+            {
+                public Integer execute() throws Exception
+                {
+                    
+                    if (!nodeService.exists(fNodeREf))
+                        return 0; // finished
+                    //before trying to prune, we test if the root none has aspect ASPECT_ARCHIVED
+                    if(!nodeService.hasAspect(fNodeREf, ContentModel.ASPECT_ARCHIVED))
+                    {
+                        //issue a warning it does not look normal and skip it
+                        logger.warn("Expected ASPECT_ARCHIVED on " + fNodeREf + " to be present. Node skipped!");
+                        fSkip.set(true);
+                        return 0; // do not delete it
+                    }
+                    //also testing ContentModel.PROP_ARCHIVED_DATE != null
+                    if(nodeService.getProperty(fNodeREf, ContentModel.PROP_ARCHIVED_DATE)== null)
+                    {
+                        //issue a warning it does not look normal and skip it
+                        logger.warn("Expected PROP_ARCHIVED_DATE on " + fNodeREf + " not null. Node skipped!");
+                        fSkip.set(true);
+                        return 0; // do not delete it
+                    }
+                    return this.deleteRecursiveInternal(fNodeREf, fMaxNumOfNodesInTransaction);
+                }
 
+                public int deleteRecursiveInternal(NodeRef nodeRef, int fMaxNumOfNodesInTransaction)
+                {
+                    if (!nodeService.exists(nodeRef))
+                        return 0; // finished
+                    List<ChildAssociationRef> allChildren = nodeService.getChildAssocs(nodeRef);
+
+                    // if no child then delete the node, this is a leaf
+                    if (allChildren == null || allChildren.size() == 0)
+                    {
+                        
+                        // maybe we need to preserve it if specific type
+                        // or specific nodeRef provided in configuration
+                        if (!mustBeProtected(nodeRef))
+                        {
+                            nodeService.deleteNode(nodeRef);
+                            return 1;
+                        }
+                        else
+                        {
+                            fSkip.set(true);
+                            return 0; // do not delete it
+                        }
+                        
+                        
+
+                    }
+
+                    // recursive an iterate and count
+                    int sum = 0;
+                    for (ChildAssociationRef assoc : allChildren)
+                    {
+                        sum += deleteRecursiveInternal(assoc.getChildRef(), fMaxNumOfNodesInTransaction);
+                        if (sum >= fMaxNumOfNodesInTransaction)
+                            break;
+                    }
+                    return sum;
+                }
+
+            };
+        return transactionService.getRetryingTransactionHelper().doInTransaction(recursiveWork, false,
+                true);
+    }
     public void execute()
     {
         String lockToken = null;
@@ -290,161 +362,58 @@ public class TrashcanCleaner
         if (this.protectedDays > 0)
         {
             final MutableInteger fToSkip = new MutableInteger(0);
-            final RetryingTransactionCallback<Boolean> pagingWork = new RetryingTransactionCallback<Boolean>()
+            
+            
+            final RetryingTransactionCallback<List<NodeRef>> getPage = new RetryingTransactionCallback<List<NodeRef>>()
+            {
+                public List<NodeRef> execute() throws Exception
                 {
-                    public Boolean execute() throws Exception
+
+                    NodeRef archiveRoot = nodeService.getRootNode(storeRef);
+                    // get canned query
+                    //It could be the sort slowing down
+                    //set org.alfresco.repo.node.getchildren.GetChildrenCannedQuery=debug to get query exec time
+                    GetChildrenCannedQueryFactory getChildrenCannedQueryFactory = (GetChildrenCannedQueryFactory) cannedQueryRegistry
+                            .getNamedObject(CANNED_QUERY_FILEFOLDER_LIST);
+
+                    List<Pair<QName, Boolean>> sortProps = new ArrayList<Pair<QName, Boolean>>();
+                    sortProps.add(new Pair<QName, Boolean>(ContentModel.PROP_ARCHIVED_DATE, true));
+
+                    Set<QName> assocTypeQNames = new HashSet<QName>(1);
+                    assocTypeQNames.add(ContentModel.ASSOC_CHILDREN);
+
+                    Set<QName> childTypeQNames = new HashSet<QName>(2);
+                    //Other custom types are purged
+                    //childTypeQNames.add(ContentModel.TYPE_CONTENT);
+                    //childTypeQNames.add(ContentModel.TYPE_FOLDER);
+
+                    PagingRequest pagingRequest = new PagingRequest(fToSkip.value, pageLen);
+
+                    GetChildrenCannedQuery cq = (GetChildrenCannedQuery) getChildrenCannedQueryFactory
+                            .getCannedQuery(archiveRoot, "*", assocTypeQNames, childTypeQNames, null, sortProps,
+                                    pagingRequest);
+                    long queryStartExecTime = System.currentTimeMillis();
+                    // execute canned query
+                    CannedQueryResults<NodeRef> results = cq.execute();
+
+                    if (logger.isDebugEnabled())
                     {
-
-                        NodeRef archiveRoot = nodeService.getRootNode(storeRef);
-                        // get canned query
-                        GetChildrenCannedQueryFactory getChildrenCannedQueryFactory = (GetChildrenCannedQueryFactory) cannedQueryRegistry
-                                .getNamedObject(CANNED_QUERY_FILEFOLDER_LIST);
-
-                        List<Pair<QName, Boolean>> sortProps = new ArrayList<Pair<QName, Boolean>>();
-                        sortProps.add(new Pair<QName, Boolean>(ContentModel.PROP_ARCHIVED_DATE, true));
-
-                        Set<QName> assocTypeQNames = new HashSet<QName>(1);
-                        assocTypeQNames.add(ContentModel.ASSOC_CHILDREN);
-
-                        Set<QName> childTypeQNames = new HashSet<QName>(2);
-                        //Other custom types are purged
-                        //childTypeQNames.add(ContentModel.TYPE_CONTENT);
-                        //childTypeQNames.add(ContentModel.TYPE_FOLDER);
-
-                        PagingRequest pagingRequest = new PagingRequest(fToSkip.value, pageLen);
-
-                        GetChildrenCannedQuery cq = (GetChildrenCannedQuery) getChildrenCannedQueryFactory
-                                .getCannedQuery(archiveRoot, "*", assocTypeQNames, childTypeQNames, null, sortProps,
-                                        pagingRequest);
-                        long queryStartExecTime = System.currentTimeMillis();
-                        // execute canned query
-                        CannedQueryResults<NodeRef> results = cq.execute();
-
-                        if (logger.isDebugEnabled())
-                        {
-                            long queryEndExecTime = System.currentTimeMillis();
-                            logger.debug("Query exec time: " + (queryEndExecTime - queryStartExecTime) + "ms");
-                            long getPageCountStart = System.currentTimeMillis();
-                            logger.debug("Number of pages available:" + results.getPageCount());
-                            long getPageCountEnd = System.currentTimeMillis();
-                            logger.debug("getPageCount() exec time :" + (getPageCountEnd - getPageCountStart) + "ms");
-                        }
-                        //by seeing the first "Delete NodeRef" we will know the getPage() time 
-                        List<NodeRef> pageElements = results.getPage();
-
-                        if (pageElements.size() == 0)
-                            return false;
-
-                        Date toDate = new Date(new Date().getTime() - (1000L * 60L * 60L * 24L * protectedDays));
-
-                        // display the page
-                        for (NodeRef nodeRef : pageElements)
-                        {
-                            Date archivedDate = (Date) nodeService
-                                    .getProperty(nodeRef, ContentModel.PROP_ARCHIVED_DATE);
-                            //here we are testing the case if some node are in archive but not having ContentModel.PROP_ARCHIVED_DATE
-                            //I also probably mean that the aspect ASPECT_ARCHIVED is not there neither.
-                            //Therefore we display a warning when aspect ASPECT_ARCHIVED is not present or PROP_ARCHIVED_DATE
-                            //is null. Given that there elements are skipped we do the tests in deleteRecursive(...)
-                            if (archivedDate != null && archivedDate.compareTo(toDate) >= 0)
-                                return false;
-                            String name = (String) nodeService.getProperty(nodeRef, ContentModel.PROP_NAME);
-                            if (logger.isDebugEnabled())
-                            {
-                                logger.debug("Delete NodeRef :" + nodeRef + " $ Name :" + name + " $ archivedDate :"
-                                        + archivedDate);
-                            }
-                            // Maybe there is more than one element to deleted there
-                            // It might be a big tree and therefore causing/requiring big or even too big transaction
-                            MutableBoolean fSkipBool = new MutableBoolean(false);
-                            while (deleteRecursive(nodeRef, 500, fSkipBool) != 0);
-                            // it true we have a tree that was pruned but containing
-                            // a node of type that must be preserved
-                            if (fSkipBool.value == true)
-                                fToSkip.increment();
-                        }
-
-                        return true;
+                        long queryEndExecTime = System.currentTimeMillis();
+                        logger.debug("Query exec time: " + (queryEndExecTime - queryStartExecTime) + "ms");
+                        long getPageCountStart = System.currentTimeMillis();
+                        logger.debug("Number of pages available:" + results.getPageCount());
+                        long getPageCountEnd = System.currentTimeMillis();
+                        logger.debug("getPageCount() exec time:" + (getPageCountEnd - getPageCountStart) + "ms");
                     }
 
-                    int deleteRecursive(NodeRef nodeRef, int maxNumOfNodesInTransaction, MutableBoolean skip)
-                    {
-                        final NodeRef fNodeREf = nodeRef;
-                        final MutableBoolean fSkip = skip;
-                        final int fMaxNumOfNodesInTransaction = maxNumOfNodesInTransaction;
-                        final RetryingTransactionCallback<Integer> recursiveWork = new RetryingTransactionCallback<Integer>()
-                            {
-                                public Integer execute() throws Exception
-                                {
-                                    
-                                    if (!nodeService.exists(fNodeREf))
-                                        return 0; // finished
-                                    //before trying to prune, we test if the root none has aspect ASPECT_ARCHIVED
-                                    if(!nodeService.hasAspect(fNodeREf, ContentModel.ASPECT_ARCHIVED))
-                                    {
-                                        //issue a warning it does not look normal and skip it
-                                        logger.warn("Expected ASPECT_ARCHIVED on " + fNodeREf + " to be present. Node skipped!");
-                                        fSkip.set(true);
-                                        return 0; // do not delete it
-                                    }
-                                    //also testing ContentModel.PROP_ARCHIVED_DATE != null
-                                    if(nodeService.getProperty(fNodeREf, ContentModel.PROP_ARCHIVED_DATE)== null)
-                                    {
-                                        //issue a warning it does not look normal and skip it
-                                        logger.warn("Expected PROP_ARCHIVED_DATE on " + fNodeREf + " not null. Node skipped!");
-                                        fSkip.set(true);
-                                        return 0; // do not delete it
-                                    }
-                                    return this.deleteRecursiveInternal(fNodeREf, fMaxNumOfNodesInTransaction);
-                                }
+                    List<NodeRef> pageElements = results.getPage();
 
-                                public int deleteRecursiveInternal(NodeRef nodeRef, int fMaxNumOfNodesInTransaction)
-                                {
-                                    if (!nodeService.exists(nodeRef))
-                                        return 0; // finished
-                                    List<ChildAssociationRef> allChildren = nodeService.getChildAssocs(nodeRef);
-
-                                    // if no child then delete the node, this is a leaf
-                                    if (allChildren == null || allChildren.size() == 0)
-                                    {
-                                        
-                                        // maybe we need to preserve it if specific type
-                                        // or specific nodeRef provided in configuration
-                                        if (!mustBeProtected(nodeRef))
-                                        {
-                                            nodeService.deleteNode(nodeRef);
-                                            return 1;
-                                        }
-                                        else
-                                        {
-                                            fSkip.set(true);
-                                            return 0; // do not delete it
-                                        }
-                                        
-                                        
-
-                                    }
-
-                                    // recursive an iterate and count
-                                    int sum = 0;
-                                    for (ChildAssociationRef assoc : allChildren)
-                                    {
-                                        sum += deleteRecursiveInternal(assoc.getChildRef(), fMaxNumOfNodesInTransaction);
-                                        if (sum >= fMaxNumOfNodesInTransaction)
-                                            break;
-                                    }
-                                    return sum;
-                                }
-
-                            };
-                        return transactionService.getRetryingTransactionHelper().doInTransaction(recursiveWork, false,
-                                true);
-                    }
-
-                };
+                    return pageElements;
+                }
+               };
 
             // final AtomicBoolean keepGoing = new AtomicBoolean(true);
-            Boolean doMore = false;
+            Boolean doMore = true;
 
             int iteration = 1;
             do
@@ -454,9 +423,49 @@ public class TrashcanCleaner
                     logger.debug("iteration =" + iteration);
                 }
                 iteration++;
-                doMore = transactionService.getRetryingTransactionHelper().doInTransaction(pagingWork, false, true);
+                List<NodeRef> pageElements = transactionService.getRetryingTransactionHelper().doInTransaction(getPage, true, true);
+                if( pageElements.size() == 0 )
+                   break;
+                Date toDate = new Date(new Date().getTime() - (1000L * 60L * 60L * 24L * protectedDays));
+                // display the page
+                for (NodeRef nodeRef : pageElements)
+                {
+                    Date archivedDate = (Date) nodeService
+                            .getProperty(nodeRef, ContentModel.PROP_ARCHIVED_DATE);
+                    //here we are testing the case if some node are in archive but not having ContentModel.PROP_ARCHIVED_DATE
+                    //I also probably mean that the aspect ASPECT_ARCHIVED is not there neither.
+                    //Therefore we display a warning when aspect ASPECT_ARCHIVED is not present or PROP_ARCHIVED_DATE
+                    //is null. Given that there elements are skipped we do the tests in deleteRecursive(...)
+                    if (archivedDate != null && archivedDate.compareTo(toDate) >= 0)
+                    {
+                        doMore = false;
+                        break;
+                    }
+                    String name = (String) nodeService.getProperty(nodeRef, ContentModel.PROP_NAME);
+                    if (logger.isDebugEnabled())
+                    {
+                        logger.debug("Delete NodeRef :" + nodeRef + " $ Name :" + name + " $ archivedDate :"
+                                + archivedDate);
+                    }
+                    // Maybe there is more than one element to deleted there
+                    // It might be a big tree and therefore causing/requiring big or even too big transaction
+                    MutableBoolean fSkipBool = new MutableBoolean(false);
+                    while (deleteRecursive(nodeRef, 500, fSkipBool) != 0)
+                    {
+                    }
+                    // it true we have a tree that was pruned but containing
+                    // a node of type that must be preserved
+                    if (fSkipBool.value == true)
+                    {
+                        fToSkip.increment();
+                    }
+                }
+
+                //doMore = true;
+                
             }
             while (doMore == true);
+            
 
         }
     }
